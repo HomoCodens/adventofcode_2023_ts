2247153f07e65687301cf1eab98c3904fef4c426
HomoCodens <homocodens@github.com>
2023-12-02 09:21:11 +0100

    day2

--- src/solvers/index.ts
+++ src/solvers/index.ts
@@ -1,5 +1,7 @@
 import SolverDay01 from './solverday01'
+import SolverDay02 from './solverday02'
 
 export {
     SolverDay01,
+    SolverDay02,
 }
---
+++ src/solvers/solverday02.ts
@@ -0,0 +1,89 @@
+import SolverBase, { Solvution } from './solverbase'
+
+class CoobRound {
+    red: number
+    green: number
+    blue: number
+
+    private constructor(red: number, green: number, blue: number) {
+        this.red = red ?? 0
+        this.green = green ?? 0
+        this.blue = blue ?? 0
+    }
+
+    static fromString(def: string): CoobRound {
+        const coobs = def.split(',')
+        const params = coobs.reduce((acc: any, x: string) => {
+            const params = x.trim().split(' ')
+            acc[params[1]] = +params[0]
+            return acc
+        }, {})
+
+        return new CoobRound(params.red, params.green, params.blue)
+    }
+
+    izLeegl(red: number, green: number, blue: number): boolean {
+        return this.red <= red && this.green <= green && this.blue <= blue
+    }
+}
+
+type GameParams = {
+    id: number,
+    rounds: string
+} | undefined
+
+class CoobGame {
+    id: number
+    rounds: CoobRound[]
+
+    private static re = /^Game (?<id>\d+):(?<rounds>( \d+ \w+[,;]?)+)$/
+
+    private constructor(id: number, rounds: CoobRound[]) {
+        this.id = id
+        this.rounds = rounds
+    }
+
+    static fromString(def: string): CoobGame {
+        const params: GameParams = CoobGame.re.exec(def)?.groups as GameParams
+        if(params) {
+            return new CoobGame(+params.id, params.rounds.split(';').map(CoobRound.fromString))
+        } else {
+            throw 'Physics still works though, right?'
+        }
+    }
+
+    izLeegl(red: number, green: number, blue: number): boolean {
+        return this.rounds.every((r) => r.izLeegl(red, green, blue))
+    }
+
+    gitMaximinPower(): number {
+        return Object.values<number>(this.rounds.reduce((acc: any, x) => {
+            return {
+                red: Math.max(acc.red, x.red),
+                green: Math.max(acc.green, x.green),
+                blue: Math.max(acc.blue, x.blue)
+            }
+        }, {
+            red: 0,
+            green: 0,
+            blue: 0
+        })).reduce((acc, x) => acc * x)
+    }
+}
+
+export default class SolverDay02 extends SolverBase<CoobGame[]> {
+    prepareInput(rawInput: string): CoobGame[] {
+        return rawInput.trim().split('\n').map(CoobGame.fromString)
+    }
+    solvePartOne(input: CoobGame[]): Solvution {
+        return new Solvution(
+            input.filter((g) => g.izLeegl(12, 13, 14)).reduce((acc: number, g: CoobGame) => acc + g.id, 0)
+        )
+    }
+    solvePartTwo(input: CoobGame[]): Solvution {
+        return new Solvution(
+            input.map((g) => g.gitMaximinPower()).reduce((acc, p) => acc + p)
+        )
+    }
+
+}
